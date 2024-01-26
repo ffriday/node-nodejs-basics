8da8b94683ed4100b25cccee8bf0053a4953ce27
mitrich <4irtim@gmail.com>
2024-01-26 11:47:16 +0700

    feat: solved Command line interface(src/cli) subtask

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const env = Object.entries(process.env)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+  console.log(env);
 };
 
 parseEnv();
