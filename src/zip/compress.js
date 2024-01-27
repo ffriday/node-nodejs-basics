7ee258206fdcb9ac26ca88d4e35dda3efd26d797
Dmitry Bulgakov <4irtim@gmail.com>
2024-01-27 12:45:34 +0700

    feat: feat: solved Zlib (src/zip) subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const rootPath = dirname(fileURLToPath(import.meta.url));
+const sourcePath = path.join(rootPath, "files", "fileToCompress.txt");
+const destPath = path.join(rootPath, "files", "archive.gz");
+
 const compress = async () => {
-    // Write your code here 
+    const src = createReadStream(sourcePath);
+    const dest = createWriteStream(destPath);
+    const gzip = createGzip();
+    pipeline(src, gzip, dest, (err) => {
+        if (err) console.log(err);
+    });
 };
 
 await compress();
