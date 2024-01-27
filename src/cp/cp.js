113d25f75b63ad48b738531d84f90f6ff092fc01
Dmitry Bulgakov <4irtim@gmail.com>
2024-01-27 19:04:41 +0700

    feat: feat: solved Child Processes (src/cp) subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { spawn } from 'node:child_process';
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const rootPath = dirname(fileURLToPath(import.meta.url));
+const childPath = path.join(rootPath, 'files', "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [childPath, ...args]);
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['fgs', 'fds']);
