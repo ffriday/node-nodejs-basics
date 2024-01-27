7a515905af0965ec5ab3381bf609848a380a58a6
Dmitry Bulgakov <4irtim@gmail.com>
2024-01-27 18:41:36 +0700

    feat: feat: solved Worker Threads (src/wt) subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { cpus } from "node:os";
+import { Worker } from "worker_threads";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const rootPath = dirname(fileURLToPath(import.meta.url));
+const workerPath = path.join(rootPath, "worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+  const cores = cpus().length;
+  const workers = Array.from({ length: cores }, (_, i) => i).map(
+    (core) => new Worker(workerPath, { workerData: core + 10 })
+  );
+
+  let count = 0;
+  workers.forEach((worker) =>
+    worker.on("message", (msg) => {
+      worker.resultData = msg;
+      count++;
+      if (count === cores) console.log(workers.map((w) => w.resultData));
+    })
+  );
 };
 
 await performCalculations();
