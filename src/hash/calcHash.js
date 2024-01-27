0a1c16e4845022ee1b248146e570305ea8c0d00d
Dmitry Bulgakov <4irtim@gmail.com>
2024-01-27 11:11:05 +0700

    feat: feat: solved Hash (src/hash) subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from "node:fs";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const rootPath = dirname(fileURLToPath(import.meta.url));
+const sourcePath = path.join(rootPath, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+    const readableStream = createReadStream(sourcePath);
+    readableStream.on('data', (data) => {
+        const hash = createHash('sha256').update(data).digest('hex');
+        console.log(hash);
+    });
 };
 
 await calculateHash();
